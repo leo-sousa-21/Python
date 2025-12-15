cbc47b2faeb34f58d9932ddfb6410e8561a35d1e
leo-sousa-21 <leo21062008@gmail.com>
2025-12-15 09:03:19 +0000

    Gera limit

---
+++ Gerador de limites.py
@@ -0,0 +1,11 @@
+LInf=int(input("Insira um valor:"))
+LSup=int(input("Insira um valor superior ao anterior:"))
+if (LInf>LSup):
+    print("O segundo valor é inferior ao anterior.")
+else:
+    while (LSup<=LInf):
+    
+        print(LInf)
+        LInf= LInf +1
+
+
