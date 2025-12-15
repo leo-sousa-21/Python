090db98b3da88ace57c84e76f7c815243d6f507c
leo-sousa-21 <leo21062008@gmail.com>
2025-12-15 09:43:29 +0000

    IMPARSI10

---
+++ ImpParSI10.py
@@ -0,0 +1,9 @@
+n=int
+for i1 in range(10,n):
+    num1 = int(input("Digite o 1º número: "))
+    res1= num1%2
+    print(res1)
+    i1=print("O valor ",num1," é par e superior 10")
+else:
+    for i1 in range(0,10):
+        i1=print("O valor ",i1," é par e inferior 10")
