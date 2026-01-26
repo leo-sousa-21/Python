9be658b5742b473cb5558219f784b483768cd36c
leo-sousa-21 <leo21062008@gmail.com>
2026-01-26 09:22:36 +0000

    novo

--- # Importação do módulo pyplot da bibliot.py
+++ # Importação do módulo pyplot da bibliot.py
@@ -1,7 +1,5 @@
 # Importação do módulo pyplot da biblioteca matplotlib
 import matplotlib.pyplot
-
-
 # Listas de valores a ser apresentados no gráfico
 matplotlib.pyplot.plot(["Jan.","Fev.","Mar.","Abr.","Mai.","Jun.","Jul.","Ago.","Set.","Out.","Nov.","Dez."],[9.03,9.78,12.37,13.96,16.78,20.39,22.54,22.92,20.48,16.84,12.28,9.75],'-o',color="purple", linewidth=3)
 
--- Matplotlib.py
+++ Matplotlib.py
@@ -1,14 +1,20 @@
 import matplotlib.pyplot
 
-matplotlib.pyplot.plot(["Jan.", "Fev.", "Mar.", "Abr.", "Maio", "Jun.", "Jul.", "Ago.",
-     "Set.", "Out.", "Nov.", "Dez."],[9.03, 9.78, 12.37, 13.99, 16.78,20.39, 22.54, 22.92, 20.48,16.84, 12.28, 9.75],"-o", color="purple", linewidth=3)
 
-matplotlib.pyplot.title("Temperatura Média 1991-2010 (Portugal Continental - Fonte IPMA)")
+# Listas de valores a ser apresentados no gráfico
+matplotlib.pyplot.plot(["Jan.","Fev.","Mar.","Abr.","Mai.","Jun.","Jul.","Ago.","Set.","Out.","Nov.","Dez."],[9.03,9.78,12.37,13.96,16.78,20.39,22.54,22.92,20.48,16.84,12.28,9.75],'-o',color="purple", linewidth=3)
 
-matplotlib.pyplot.ylabel("Temperatura (ºC)")
+# Apresenta o título do gráfico
+matplotlib.pyplot.title('Temperatura Média 1991-2019(Portugal Continental-Fonte IPMA)')
 
-matplotlib.pyplot.xlabel("Mês")
+# Apresenta o rótulo do eixo do y
+matplotlib.pyplot.ylabel('Temperatura (ºC)')
 
+# Apresenta o rótulo do eixo do x
+matplotlib.pyplot.xlabel('Mês')
+
+# Apresenta a grelha do gráfico
 matplotlib.pyplot.grid()
 
+# Apresenta o gráfico numa janela
 matplotlib.pyplot.show()
