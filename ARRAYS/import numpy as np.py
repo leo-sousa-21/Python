ffaaafe621a004d310c21f684eea381421eff917
leo-sousa-21 <leo21062008@gmail.com>
2026-02-06 14:50:58 +0000

    sed

---
+++ ARRAYS/import numpy as np.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+print('Serras de Portugal')
+serras = np.array(["Estrela", "Monchique"])
+print(serras)
+
+print('Portugal em km')
+portugal_km = np.array((561, 218, 832, 1215), dtype=float)
+print(portugal_km)
